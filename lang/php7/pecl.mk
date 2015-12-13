c0b0c242bc3ea0611e45bda919ecc59df6a46f63
「有事燒紙」 <cmheia@gmail.com>
2015-12-13 20:59:33 +0800

    A blob of makefiles
    share my work, some mk like nginx, php7 and baidupcs.

---
+++ lang/php7/pecl.mk
@@ -0,0 +1,49 @@
+#
+# Copyright (C) 2011-2015 OpenWrt.org
+#
+# This is free software, licensed under the GNU General Public License v2.
+# See /LICENSE for more information.
+#
+
+define Package/php7-pecl/Default
+  SUBMENU:=PHP
+  SECTION:=lang
+  CATEGORY:=Languages
+  URL:=http://pecl.php.net/
+  DEPENDS:=php7
+endef
+
+define Build/Prepare
+	$(Build/Prepare/Default)
+	# DEBUG: run phpize
+	( cd $(PKG_BUILD_DIR); $(STAGING_DIR_HOST)/opt/bin/phpize )
+endef
+
+CONFIGURE_ARGS+= \
+  --with-php-config=$(STAGING_DIR_HOST)/opt/bin/php-config
+
+define PECLPackage
+
+  define Package/php7-pecl-$(1)
+    $(call Package/php7-pecl/Default)
+    TITLE:=$(2)
+
+    ifneq ($(3),)
+      DEPENDS+=$(3)
+    endif
+  endef
+
+  define Package/php7-pecl-$(1)/install
+	# DEBUG: PECLPackage install
+	$(INSTALL_DIR) $$(1)/opt/lib/php
+	$(INSTALL_BIN) $(PKG_BUILD_DIR)/modules/$(subst -,_,$(1)).so $$(1)/opt/lib/php/
+
+	$(INSTALL_DIR) $$(1)/opt/etc/php7
+    ifeq ($(4),zend)
+	echo "zend_extension=/opt/lib/php/$(subst -,_,$(1)).so" > $$(1)/opt/etc/php7/$(subst -,_,$(1)).ini
+    else
+	echo "extension=$(subst -,_,$(1)).so" > $$(1)/opt/etc/php7/$(subst -,_,$(1)).ini
+    endif
+  endef
+
+endef
